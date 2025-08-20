e5d48cd34355facc84f1a87f2abc68dac9d815e6
CaoQuyBinh <denji0908@gmail.com>
2025-08-20 22:59:49 +0700

    cập nhật home và thêm các folder MVC khác

--- views/home.py
+++ views/home.py
@@ -1,79 +1,49 @@
 from tkinter import *
 import subprocess
-from PIL import Image, ImageTk
-# from Apl import QuanLyDichVu
-# from Apl import QuanLyNguoiThue
-# from Apl import QuanLyPhongTro
-# from Apl import QuanLyNhanVien
-# from Apl import QuanLyHopDong
-
 
 def trang_chu():
     window.destroy()
-    subprocess.run(["python", "home.py"])
+    subprocess.run(["python", "../views/home.py"])
 
 def dang_xuat():
     window.destroy()
-    subprocess.run(["python", "login.py"])
+    subprocess.run(["python", "../views/login.py"])
 
 def thoat():
     exit()
 
-def kich_thuoc(event=None):
-    dai = window.winfo_width()
-    cao = window.winfo_height()
-    frame_menu.place(x=0, y=0, width=250, height=cao)
-    frame_main.place(x=250, y=0, width=dai - 250, height=cao)
-    frame_option.place(x=850, y=0, width=dai - 250, height=cao)
-    label_tieude.place(x=(dai - 250) // 2 - 200, y=10)
-    frame_gioithieu.place(x=20, y=50, width=dai - 290, height=cao - 100)
-    label_gt3.place(x=(dai - 250) // 2 - 150, y=20)
-    label_gt2.place(x=(dai - 250) // 2 - 330, y=60)
+def size(event=None):
+    width = window.winfo_width()
+    height = window.winfo_height()
+    frame_menu.place(x=0, y=0, width=220, height=height)
+    frame_main.place(x=170, y=0, width=width - 620, height=height)
+    frame_option.place(x=1050, y=0, width=width - 250, height=height)
+    label_1.place(x=(width - 400) // 2 - 200, y=10)
+    # label_2.place(x=200, y=50)
+    frame_gioithieu.place(x=20, y=50, width=width - 700, height=height - 100)
+    frame_order.place(x=0, y=50, width=width - 1070, height=height - 100)
 
-    SL_buttons = len(buttons)
-    khoang_cach_button = (cao - 100) // (SL_buttons + 1) + 5
+    num_buttons = len(buttons)
+    gap_button = (height - 100) // (num_buttons + 1) + 5
     for i, button in enumerate(buttons):
-        button.place(x=20, y=(i + 1) * khoang_cach_button, width=200, height=40)
+        button.place(x=20, y=(i + 1) * gap_button, width=150, height=40)
 
 window = Tk()
 window.title("Cafe femboy")
-window.geometry("1250x700")
-window.configure(bg="Alice Blue")
-# window.iconbitmap("../Picture_Icon/icon.ico")
+window.state('zoomed')
+# window.iconbitmap("../Img/icon.ico")
 
-frame_menu = Frame(window, bg="Powder Blue")
-frame_main = Frame(window, bg="#F0F8FF")
-frame_option = Frame(window, bg="#808080")
+frame_menu = Frame(window, bg="#A9A9A9")
+frame_main = Frame(window, bg="#A9A9A9")
+frame_option = Frame(window, bg="#A9A9A9")
 
-label_tieude = Label(frame_main, text="ỨNG DỤNG QUẢN LÝ PHÒNG TRỌ", font=("Times New Roman", 20, "bold"), fg="#89CFF0", bg="#F0F8FF")
+label_1 = Label(frame_main, text="ỨNG DỤNG QUẢN LÝ QUÁN CAFE", font=("Times New Roman", 20, "bold"), fg="#000000", bg="#A9A9A9")
+#label_2 = Label(frame_main, text="THÔNG TIN HÓA ĐƠN", font=("Times New Roman", 20, "bold"), fg="#000000", bg="#A9A9A9")
 frame_gioithieu = Frame(frame_main, bg="white", bd=3, relief="solid")
+frame_order = Frame(frame_option, bg="white", bd=3, relief="solid")
 
 label_noidung = Label(frame_gioithieu, text="", font=("Arial", 14), fg="black", bg="white")
 label_noidung.pack(pady=10)
-
-# img = Image.open("../Picture_Icon/bg1.png")
-# img = img.resize((1000, 660), Image.Resampling.LANCZOS)
-# bg_image = ImageTk.PhotoImage(img)
-# label_bg = Label(frame_gioithieu, image=bg_image)
-# label_bg.place(relwidth=1, relheight=1)
-
-text_gt3 = "Phần mềm quản lý cho thuê"
-label_gt3 = Label(frame_gioithieu, text="", font=("Arial", 16, "bold"), fg="red", bg="white")
-
-def gt3(index=0):
-    if index < len(text_gt3):
-        label_gt3.config(text=text_gt3[:index + 1])
-        window.after(250, gt3, index + 1)
-gt3()
-
-text_gt2 = "Hiệu quả - Chuyên nghiệp - Tránh sai sót... Quản lý chưa bao giờ dễ dàng hơn thế!"
-label_gt2 = Label(frame_gioithieu, text="", font=("Arial", 14), fg="darkblue", bg="white")
-
-def gt2(index=0):
-    if index < len(text_gt2):
-        label_gt2.config(text=text_gt2[:index + 1])
-        window.after(80, gt2, index + 1)
-gt2()
 
 buttons = []
 buttons.append(Button(frame_menu, text="Trang Chủ", font=("Times New Roman", 12)))
@@ -92,5 +62,5 @@ buttons.extend([
     Button(frame_menu, text="Thoát ứng dụng", bg="#FF6666", fg="white", font=("Times New Roman", 12), command=thoat)
 ])
 
-window.bind("<Configure>", kich_thuoc)
+window.bind("<Configure>", size)
 window.mainloop()
