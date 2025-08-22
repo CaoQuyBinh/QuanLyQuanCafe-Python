244b913a618a9d15958e9a0a52f2e0299ac4f8c5
CaoQuyBinh <denji0908@gmail.com>
2025-08-22 16:01:56 +0700

    thêm một số models

---
+++ models/Supplier.py
@@ -0,0 +1,6 @@
+class Supplier:
+    def __init__(self, sid, sName, pName, pQuantity):
+        self.sID = sid
+        self.sName = sName
+        self.pName = pName
+        self.pQuantity = pQuantity
