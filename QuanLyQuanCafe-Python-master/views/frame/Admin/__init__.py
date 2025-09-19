36233937234aad409b009b38d6d138fdf60f6286
CaoQuyBinh <denji0908@gmail.com>
2025-09-19 14:31:27 +0700

    Delete QuanLyQuanCafe-Python-master directory

--- QuanLyQuanCafe-Python-master/views/frame/Admin/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Products import gui_product
-from .Storage import gui_storage
-from .NCC import gui_ncc
-from .Employees import gui_employees
-from .Accounts import gui_account
-from .Bills import gui_bill
--- QuanLyQuanCafe-Python-master/views/frame/User/__init__.py
+++
@@ -1,3 +0,0 @@
-from .Products import gui_product2
-from .Storage import gui_storage2
-from .Bills import gui_bill2
